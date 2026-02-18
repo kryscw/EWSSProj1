89cd9ff040365a5ebd71cd346500e57f14b856bc
kryscw <ethandubau@gmail.com>
2026-02-18 16:12:23 -0600

    Added date and time; scrollbox added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let listObjectPrefix = "<li>"
 let listObjectSuffix = "</li>"
 let listArray = [];
 let listCheckedArray = [];
+let listDateArray = [];
 
 let todoList = document.getElementById("todoList")
 let todoInput = document.getElementById("todoInput")
@@ -9,6 +10,17 @@ let todoInput = document.getElementById("todoInput")
 function addItem() {
     listArray.push(todoInput.value);
     listCheckedArray.push("");
+    
+    let currentDate = new Date();
+    // I have no idea if there's a better way to do format time lmao
+    let additionalZeroMinutes = "";
+    let additionalZeroSeconds = "";
+    let additionalZeroHours = "";
+    if (currentDate.getUTCMinutes() < 10) { additionalZeroMinutes = "0"; }
+    if (currentDate.getUTCSeconds() < 10) { additionalZeroSeconds = "0"; }
+    if (currentDate.getUTCHours() < 10) { additionalZeroHours = "0"; }
+    listDateArray.push(additionalZeroHours + currentDate.getHours() + ":" + additionalZeroMinutes + currentDate.getUTCMinutes() + ":" + additionalZeroSeconds + currentDate.getUTCSeconds());
+    
     updateList();
 }
 
@@ -28,7 +40,8 @@ function updateList() {
             "   <input type='checkbox' onclick='check(this," + i + ")'" + listCheckedArray[i] + ">  " +
             listArray[i] +
             "   <button onclick='deleteItem(" + i + ")'> X </button>" +
-            listObjectSuffix
+            listObjectSuffix +
+            listDateArray[i]
     }
 }
 
