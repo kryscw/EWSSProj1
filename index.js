a0032831c8a59f49096a0bb13882bd82269faaac
kryscw <ethandubau@gmail.com>
2026-02-18 13:08:50 -0600

    The List™

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-console.log("Hello World!");
+let listObjectPrefix = "<li>"
+let listObjectSuffix = "</li>"
+let listArray = [];
+let listCheckedArray = [];
+
+let todoList = document.getElementById("todoList")
+let todoInput = document.getElementById("todoInput")
+
+function addItem() {
+    listArray.push(todoInput.value);
+    listCheckedArray.push("");
+    updateList();
+}
+
+function check(box, id) {
+    if (box.checked) {
+        listCheckedArray[id] = "checked='checked'";
+    } else {
+        listCheckedArray[id] = "";
+    }
+}
+
+function updateList() {
+    todoList.innerHTML = '';
+    for (let i = 0; i < listArray.length; i++) {
+        todoList.innerHTML +=
+            listObjectPrefix +
+            "   <input type='checkbox' onclick='check(this," + i + ")'" + listCheckedArray[i] + ">  " +
+            listArray[i] +
+            "   <button onclick='deleteItem(" + i + ")'> X </button>" +
+            listObjectSuffix
+    }
+}
+
+function deleteItem(id) {
+    listArray.splice(id, 1);
+    listCheckedArray.splice(id, 1);
+    updateList();
+}
